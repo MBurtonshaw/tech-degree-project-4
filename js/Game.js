8a89b68d01b4d7d6f543b0094edf8ba5905f5f91
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-01-11 14:59:30 -0800

    Made Game class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase("buy local"),
+            new Phrase("delete your gram"),
+            new Phrase("stop the filters"),
+            new Phrase("start recycling"),
+            new Phrase("read more")
+        ];
+        this.activePhrase = null;
+    }
+    getRandomPhrase() {
+        return this.phrases[Math.floor(Math.random * this.phrases.length)];
+    }
+}
