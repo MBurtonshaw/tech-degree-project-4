6b233541c32dd96dfdcd3f09b8d9949c41a71065
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-01-14 05:13:57 -0800

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,22 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
+    addPhraseToDisplay() {
+        return this.game.getRandomPhrase();
+    }
 }
+
+/*
+        let display = document.createElement("DIV");
+        display.id = "display";
+        display.class = "section";
+        let ul = document.createElement("UL");
+        document.getElementById("phrase").appendChild(ul);
+        getRandomPhrase();
+        phrase.forEach(letter => {
+            let li = document.createElement("LI");
+            li.textContent = letter;
+            ul.append(li);
+            li.addClass("letter");
+            });
+            */
