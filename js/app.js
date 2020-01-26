007f855c68348a7f660dee10c0d903151fd73124
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-01-26 10:13:15 -0800

    Keydown event working

--- js/app.js
+++ js/app.js
@@ -13,23 +13,17 @@ startButton.addEventListener("click", () => {
 
 const screenKeys = document.querySelectorAll(".key");
 
-for (let i = 0; i < screenKeys.length; i++) {
-    const newKeys = screenKeys[i];
-}
-
 screenKeys.forEach(letter => {
     letter.addEventListener("click", e => {
         game.handleInteraction(e.target);
     });
 });
 document.addEventListener("keydown", e => {
-    game.handleInteraction(e.target);
-});
-/*
-for (let i = 0; i < screenKeys.length; i++) {
-    document.addEventListener("keydown", e => {
-        if (screenKeys[i].textContent === this.ActivePhrase) {
-       console.log('yatta'); 
+    if (event.keyCode > 64 && event.keyCode < 91) {
+        for (let i = 0; i < screenKeys.length; i++) {
+            if (screenKeys[i].textContent === event.key) {
+                game.handleInteraction(screenKeys[i]);
+            }
+        }
     }
 });
-     }*/
