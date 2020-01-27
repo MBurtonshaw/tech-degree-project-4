e7e600b6151d99fa3efe87fd927829ed7861aee6
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-01-26 17:16:23 -0800

    Added comments

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
 let game;
 const startButton = document.getElementById("btn__reset");
 
+//Event listener for start button
 startButton.addEventListener("click", () => {
     game = new Game();
     game.resetGame();
@@ -13,11 +14,14 @@ startButton.addEventListener("click", () => {
 
 const screenKeys = document.querySelectorAll(".key");
 
+//Event listener for clicking onscreen keyboard
 screenKeys.forEach(letter => {
     letter.addEventListener("click", e => {
         game.handleInteraction(e.target);
     });
 });
+
+//Event listener for physical keyboard
 document.addEventListener("keydown", e => {
     if (event.keyCode > 64 && event.keyCode < 91) {
         for (let i = 0; i < screenKeys.length; i++) {
